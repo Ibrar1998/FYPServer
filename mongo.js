d4326806bbdfd320a35439e916234b379457271a
ibrar_gill <58860119+Ibrar1998@users.noreply.github.com>
2021-06-28 21:45:31 +0500

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -3,4 +3,5 @@ const mongodbErrorHandler = require('mongoose-mongodb-errors');
 mongoose.Promise=global.Promise;
 
 mongoose.plugin(mongodbErrorHandler);
-mongoose.connect("mongodb://localhost:27017/TrafficAppDb",{ useNewUrlParser: true,useUnifiedTopology: true  });
+mongoose.connect("mongodb+srv://Ibrar__Gill:mongodb123@cluster0.nbpd8.mongodb.net/TrafficAppDb?retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology: true  })
+.then(() => console.log( 'Database Connected' ));
